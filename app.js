c58fbc6604ca6d29152c5b81fab482f76728d525
David Lescano <lescanodavid07@gmail.com>
2025-08-28 22:41:02 -0300

    variables & scopes

--- app.js
+++ app.js
@@ -1,4 +1,60 @@
-function renameElement() {
-    const element = document.getElementById("titulo");
-    element.innerText = "Texto modificado desde el archivo de JavaScript";
+// Varibles
+
+// VAR
+
+// Declarando la variable
+// case sensitive
+// camelCase -> varibleRecomendadaComoEstaAhora
+// PascalCase -> VariablePascalCase
+// snake_case -> variables_snake_case
+// Tiene que referirse al contenido y tiene que ser represetativo del contenido
+// Siempre en ingles
+// No puede empezar con numeros
+
+var varibleNoRecomendada;
+
+// Asigacion
+varibleNoRecomendada = "Esta variable no se recomienda usar";
+
+var declararAsignar = "Declarado y asignando valor";
+
+var varibleNoRecomendada = "Esta var es la recomendada";
+
+// Tipo LET
+let variableRecomendada = "Esta variable tipo let es la recomendada";
+// Redeclarar - No se puede
+//let variableRecomendada;
+
+// Reasignar
+variableRecomendada = "nuevo valor";
+
+// Tipo CONST
+const usuario = "Ema";
+ // No puedo Redeclarar
+//let usuario;
+
+// No puedo reasignar
+// usuario = "David";
+
+
+// SCOPES
+
+// Global (padre)
+let edad;
+
+edad = 32;
+{
+    // Local (hijo)
+    let userName = "Ema";
+    let edad = 37;
+    //console.log("Local: ", userName, edad);
 }
+
+//console.log("Global: ", edad);
+
+
+
+
+
+
+
